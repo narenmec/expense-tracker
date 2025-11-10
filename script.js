1bed8c688141a2680f67fc32ca4d95fd0c86233d
narenmec <naren.mec@act.edu.in>
2025-11-10 06:28:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// === Firebase Setup ===
-const firebaseConfig = {
-  apiKey: "AIzaSyAO96bxbGfz7cpwK2egALEZFL1sgzDa6AA",
-  authDomain: "expensetracker-bfe87.firebaseapp.com",
-  databaseURL: "https://expensetracker-bfe87-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "expensetracker-bfe87",
-  storageBucket: "expensetracker-bfe87.firebasestorage.app",
-  messagingSenderId: "784239033436",
-  appId: "1:784239033436:web:5f3c4a53f5d4287d20fa99"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
 // === DOM Elements ===
 const fileInput = document.getElementById("excelFile");
 const uploadBtn = document.getElementById("uploadBtn");
@@ -169,3 +155,4 @@ function showChart(data) {
 }
 // === Load Last Upload Time on Page Load ===
 window.addEventListener("DOMContentLoaded", fetchLastUploadTime);
+
